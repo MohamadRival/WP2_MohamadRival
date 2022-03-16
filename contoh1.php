83304b39f3619d030a2f0f3eb7553bd4e2ed6e44
MohamadRival <Mohamadrival3@gmail.com>
2022-03-16 10:33:48 +0700

    menambahkan contoh1.php

---
+++ contoh1.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+class contoh1 extends BaseController
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama Saya Mohamad Rival,
+            saya tinggal di jakarta barat <br>
+            olahraga yang saya sukai futsal";
+    }
+}
