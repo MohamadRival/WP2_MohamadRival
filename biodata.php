8a2a120fe78849a73462749518e24130d1393401
MohamadRival <Mohamadrival3@gmail.com>
2022-03-25 21:03:54 +0700

    menambahkan file biodata.php

---
+++ biodata.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+class biodata extends BaseController
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama Saya Mohamad Rival <br> Umur saya 21 tahun <br>
+            saya tinggal di jakarta barat <br>
+            olahraga yang saya sukai";
+    }
+}
